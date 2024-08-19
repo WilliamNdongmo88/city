67870872f92486f36cada0c9bdb8f4697aef039c
unknown <WilliamNdongmo88>
2024-08-19 19:14:09 +0200

    commit city

--- city.js
+++ city.js
@@ -14,16 +14,16 @@ app.use((req, res, next) => {
   
   app.use((req, res) => {
     res.json([
-        { "id": 1, "nom": "Bafoussam", "population": 285000, "departement": "Menoua" },
-        { "id": 2, "nom": "Dschang", "population": 120000, "departement": "Menoua" },
-        { "id": 3, "nom": "Mbouda", "population": 90000, "departement": "Bamboutos" },
-        { "id": 4, "nom": "Bangangté", "population": 65000, "departement": "Ndé" },
-        { "id": 5, "nom": "Foumban", "population": 100000, "departement": "Noun" },
-        { "id": 6, "nom": "Foumbot", "population": 55000, "departement": "Noun" },
-        { "id": 7, "nom": "Kékem", "population": 35000, "departement": "Haut-Nkam" },
-        { "id": 8, "nom": "Makénéné", "population": 28000, "departement": "Haut-Nkam" },
-        { "id": 9, "nom": "Bangou", "population": 20000, "departement": "Méfi" },
-        { "id": 10, "nom": "Bafang", "population": 60000, "departement": "Haut-Nkam" }
+        { "id": 1, "nom": "Bafoussam", "population": 285000, "departement": "Menoua", "isCovered": true },
+        { "id": 2, "nom": "Dschang", "population": 120000, "departement": "Menoua", "isCovered": true },
+        { "id": 3, "nom": "Mbouda", "population": 90000, "departement": "Bamboutos", "isCovered": true },
+        { "id": 4, "nom": "Bangangté", "population": 65000, "departement": "Ndé", "isCovered": true },
+        { "id": 5, "nom": "Foumban", "population": 100000, "departement": "Noun", "isCovered": false },
+        { "id": 6, "nom": "Foumbot", "population": 55000, "departement": "Noun", "isCovered": true },
+        { "id": 7, "nom": "Kékem", "population": 35000, "departement": "Haut-Nkam", "isCovered": true },
+        { "id": 8, "nom": "Makénéné", "population": 28000, "departement": "Haut-Nkam", "isCovered": false },
+        { "id": 9, "nom": "Bangou", "population": 20000, "departement": "Méfi", "isCovered": false },
+        { "id": 10, "nom": "Bafang", "population": 60000, "departement": "Haut-Nkam", "isCovered": true }
     ]); 
  });
 
